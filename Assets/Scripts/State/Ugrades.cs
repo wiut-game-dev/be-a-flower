5a1879c038f4eaff44d24f1c8f76f8702f8da6af
Kkkiittt <khamidov357@gmail.com>
2025-08-23 15:51:21 +0500

    not stem, but root

--- Assets/Scripts/State/Ugrades.cs
+++ Assets/Scripts/State/Ugrades.cs
@@ -1,27 +1,27 @@
 public partial class UpgradeState
 {
-	public void AddStem()
+	public void AddRoot()
 	{
-		AvailableStem.Add(new(5, "st1", "Steam Upgrade", "Just better"));
-		AvailableStem[0].AddChange(BaseVariable.BaseWaterSupply, 2);
-		AvailableStem[0].AddChange(BaseVariable.BaseWaterStorage, 5);
-		AvailableStem[0].AddChange(BaseVariable.BaseNutrientStorage, 5);
-		AvailableStem.Add(new(5, "st2", "Steam Upgrade", "Just better"));
-		AvailableStem[1].AddChange(BaseVariable.BaseWaterSupply, 2);
-		AvailableStem[1].AddChange(BaseVariable.BaseWaterStorage, 5);
-		AvailableStem[1].AddChange(BaseVariable.BaseNutrientStorage, 5);
-		AvailableStem.Add(new(5, "st3", "Steam Upgrade", "Just better"));
-		AvailableStem[2].AddChange(BaseVariable.BaseWaterSupply, 2);
-		AvailableStem[2].AddChange(BaseVariable.BaseWaterStorage, 5);
-		AvailableStem[2].AddChange(BaseVariable.BaseNutrientStorage, 5);
-		AvailableStem.Add(new(5, "st4", "Steam Upgrade", "Just better"));
-		AvailableStem[3].AddChange(BaseVariable.BaseWaterSupply, 2);
-		AvailableStem[3].AddChange(BaseVariable.BaseWaterStorage, 5);
-		AvailableStem[3].AddChange(BaseVariable.BaseNutrientStorage, 5);
-		AvailableStem.Add(new(5, "stS", "Steam Super", "Much better"));
-		AvailableStem[4].AddChange(BaseVariable.BaseWaterSupply, 5);
-		AvailableStem[4].AddChange(BaseVariable.BaseWaterStorage, 10);
-		AvailableStem[4].AddChange(BaseVariable.BaseNutrientStorage, 10);
-		AvailableStem[4].AddChange(ShareVariable.WaterSupply, 0.2f);
+		AvailableRoot.Add(new(5, "st1", "Steam Upgrade", "Just better"));
+		AvailableRoot[0].AddChange(BaseVariable.BaseWaterSupply, 2);
+		AvailableRoot[0].AddChange(BaseVariable.BaseWaterStorage, 5);
+		AvailableRoot[0].AddChange(BaseVariable.BaseNutrientStorage, 5);
+		AvailableRoot.Add(new(5, "st2", "Steam Upgrade", "Just better"));
+		AvailableRoot[1].AddChange(BaseVariable.BaseWaterSupply, 2);
+		AvailableRoot[1].AddChange(BaseVariable.BaseWaterStorage, 5);
+		AvailableRoot[1].AddChange(BaseVariable.BaseNutrientStorage, 5);
+		AvailableRoot.Add(new(5, "st3", "Steam Upgrade", "Just better"));
+		AvailableRoot[2].AddChange(BaseVariable.BaseWaterSupply, 2);
+		AvailableRoot[2].AddChange(BaseVariable.BaseWaterStorage, 5);
+		AvailableRoot[2].AddChange(BaseVariable.BaseNutrientStorage, 5);
+		AvailableRoot.Add(new(5, "st4", "Steam Upgrade", "Just better"));
+		AvailableRoot[3].AddChange(BaseVariable.BaseWaterSupply, 2);
+		AvailableRoot[3].AddChange(BaseVariable.BaseWaterStorage, 5);
+		AvailableRoot[3].AddChange(BaseVariable.BaseNutrientStorage, 5);
+		AvailableRoot.Add(new(5, "stS", "Steam Super", "Much better"));
+		AvailableRoot[4].AddChange(BaseVariable.BaseWaterSupply, 5);
+		AvailableRoot[4].AddChange(BaseVariable.BaseWaterStorage, 10);
+		AvailableRoot[4].AddChange(BaseVariable.BaseNutrientStorage, 10);
+		AvailableRoot[4].AddChange(ShareVariable.WaterSupply, 0.2f);
 	}
 }
--- Assets/Scripts/Visual/UpgradeShower.cs
+++ Assets/Scripts/Visual/UpgradeShower.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class UpgradeShower : MonoBehaviour
 {
 	public TMP_Text Stem;
+	public TMP_Text Root;
+	public TMP_Text Leaf;
+	public TMP_Text Flower;
 	public TMP_Text Message;
 
 	public UpgradeState upgradeState;
@@ -14,6 +17,9 @@ public class UpgradeShower : MonoBehaviour
 	{
 		//firstly, show all possible upgrades of aspects
 		ShowAvailableStem();
+		ShowAvailableFlower();
+		ShowAvailableRoot();
+		ShowAvailableLeaf();
 	}
 
 	private void Update()
@@ -24,6 +30,21 @@ public class UpgradeShower : MonoBehaviour
 			Debug.Log("S up");//for debug purposes
 			UpgradeStem();
 		}
+		else if(Input.GetKeyUp(KeyCode.R))
+		{
+			Debug.Log("R up");//for debug purposes
+			UpgradeRoot();
+		}
+		else if(Input.GetKeyUp(KeyCode.L))
+		{
+			Debug.Log("L up");//for debug purposes
+			UpgradeLeaf();
+		}
+		else if(Input.GetKeyUp(KeyCode.F))
+		{
+			Debug.Log("F up");//for debug purposes
+			UpgradeFlower();
+		}
 	}
 
 	public void ShowAvailableStem()
@@ -33,6 +54,24 @@ public class UpgradeShower : MonoBehaviour
 		Stem.text = $"{upg.Cost}|{upg.Name} - {upg.Description}";
 	}
 
+	public void ShowAvailableRoot()
+	{
+		Upgrade upg = upgradeState.Next(Aspect.Root);
+		Root.text = $"{upg.Cost}|{upg.Name} - {upg.Description}";
+	}
+
+	public void ShowAvailableLeaf()
+	{
+		Upgrade upg = upgradeState.Next(Aspect.Leaves);
+		Leaf.text = $"{upg.Cost}|{upg.Name} - {upg.Description}";
+	}
+
+	public void ShowAvailableFlower()
+	{
+		Upgrade upg = upgradeState.Next(Aspect.Flowers);
+		Flower.text = $"{upg.Cost}|{upg.Name} - {upg.Description}";
+	}
+
 	public void UpgradeStem()
 	{
 		//try to upgrade stem
@@ -40,6 +79,54 @@ public class UpgradeShower : MonoBehaviour
 		{
 			//if yes, say so and update available upgrade
 			ShowAvailableStem();
+			Message.text = "Successful update";
+		}
+		else
+		{
+			//if no, say so
+			Message.text = "Can not upgrade";
+		}
+	}
+
+	public void UpgradeRoot()
+	{
+		//try to upgrade root
+		if(upgradeState.UpgradeNext(generalState, Aspect.Root))
+		{
+			//if yes, say so and update available upgrade
+			ShowAvailableRoot();
+			Message.text = "Successful update";
+		}
+		else
+		{
+			//if no, say so
+			Message.text = "Can not upgrade";
+		}
+	}
+
+	public void UpgradeLeaf()
+	{
+		//try to upgrade leaf
+		if(upgradeState.UpgradeNext(generalState, Aspect.Leaves))
+		{
+			//if yes, say so and update available upgrade
+			ShowAvailableLeaf();
+			Message.text = "Successful update";
+		}
+		else
+		{
+			//if no, say so
+			Message.text = "Can not upgrade";
+		}
+	}
+
+	public void UpgradeFlower()
+	{
+		//try to upgrade flower
+		if(upgradeState.UpgradeNext(generalState, Aspect.Flowers))
+		{
+			//if yes, say so and update available upgrade
+			ShowAvailableFlower();
 			Message.text = "Successful update";
 		}
 		else
